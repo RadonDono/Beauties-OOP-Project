3f331436528e59b472fb27f37effb8121000345f
Reza <130154304+RadonDono@users.noreply.github.com>
2023-06-04 06:01:27 -0700

    Add files via upload

---
+++ user_view.java
@@ -0,0 +1,22 @@
+public class user_view {
+    //comment ha dar in bakhsh be karbar nemayesh midahim
+    void wellcome(){
+        System.out.println("salam be snapfood asli khosh amadid!!!!");
+    }
+    void vorod_kardan(){
+        System.out.println("dar sorat dashtan acount adad 1 va dar gheir in soarat adad 0 ra vared konid");
+    }
+    void eror_inavalid(){
+        System.out.println("Inavalid command!");
+    }
+    void enter_username(){
+        System.out.println("username khod ra vared konid");
+    }
+    void enter_password(){
+        System.out.println("password ra vared konid");
+    }
+    void eror_password(){
+        System.out.println("format vared shode sahih nemibashad");
+        System.out.println("password bayad shamel ham adad va horof bashad");
+    }
+}
