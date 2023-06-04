d8d2c57409abaf8d2d7c992f72454e8b894b1670
Reza <130154304+RadonDono@users.noreply.github.com>
2023-06-04 06:20:27 -0700

    Add files via upload

---
+++ map.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+//show_map
+//give location
+//getlocation
+//show path
+public class map {
+    //tedad ras ha va yal haye ma
+    int rasn;
+    int yaln;
+    ArrayList<ras> rases=new ArrayList<>();
+    File file =new File("C:\\Users\\Nik\\IdeaProjects\\testmap\\src\\graph.txt");
+    map(){
+        try {
+            Scanner scanmap=new Scanner(file);
+            rasn=scanmap.nextInt();
+            sakht_raas();
+            yaln=scanmap.nextInt();
+            for (int i = 0; i <yaln ; i++) {
+                int ras1=scanmap.nextInt();
+                int ras2=scanmap.nextInt();
+                int vazn=scanmap.nextInt();
+                rases.get(ras1-1).add_yal(rases.get(ras2-1),vazn);
+                rases.get(ras2-1).add_yal(rases.get(ras1-1),vazn);
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e);
+        }
+    }
+    void sakht_raas(){
+        for (int i = 0; i < rasn; i++) {
+            rases.add(new ras(i+1));
+        }
+    }
+    //in tabe be shoma location ra bar migardanad
+    //jens an ra bayad az noe ras beszid ke yek noghte ast
+    ras get_location(int number){
+        return rases.get(number-1);
+    }
+    //shoma ras(location) ra midahid shomare ra migirid
+
+    int give_location(ras r){
+        return r.number;
+    }
+
+    //in tabe be shoma tamam noghat+etasalat ra midahad
+
+    ArrayList<ras> show_map(){
+        return rases;
+    }
+
+    //vorodi in do tabe do ras ya haman location mored nazar shomast
+    //in tabe be shoma yek kelas masir midahad
+    path show_pass(ras mabda,ras maghsad){
+        path p=new path(mabda,maghsad,rasn);
+        p.best_path();
+        p.show_path();
+        return p;
+    }
+}
