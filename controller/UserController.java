ba7f79a697b6ce3fe59affe099eab9dfea75a742
erfannaderi94 <130154310+erfannaderi94@users.noreply.github.com>
2023-07-12 16:25:46 +0330

    Add files via upload

--- controller/UserController.java
+++ controller/UserController.java
@@ -1,6 +1,11 @@
 package controller;
+import Order.Restaurant;
+import enums.Message;
+
+import java.util.ArrayList;
 
 public class UserController {
+    public Restaurant restaurant_now=null;
     private static UserController instance = null;
 
     private UserController() {
@@ -19,4 +24,46 @@ public class UserController {
         return UserController.instance;
     }
 
+    public ArrayList<String> searchrestaurant(String name) {
+        ArrayList<String> x=new ArrayList<>();
+        for (int i = 0; i < Restaurant.restaurants.size(); i++) {
+            if (Restaurant.restaurants.get(i).getName().equals(name)){
+                x.add(Restaurant.restaurants.get(i).toString());
+            }
+        }
+        return x;
+    }
+
+    public Message selectrestaurant(String ID) {
+        restaurant_now=Restaurant.getRestaurant(Integer.parseInt(ID));
+        if(Restaurant.getRestaurant(Integer.parseInt(ID))==null){
+            return Message.NoRestaurant;
+        }
+        else{
+            return Message.SUCCESS;
+        }
+    }
+
+
+
+    public ArrayList<String> searchfood(String username) {
+        ArrayList<String> x=new ArrayList<>();
+        for (int i = 0; i < Restaurant.restaurants.size(); i++) {
+            if (restaurant_now.activeFoods.get(i).getName().equals(username)){
+                x.add(Restaurant.restaurants.get(i).toString());
+            }
+        }
+        return x;
+    }
+
+    public Message selectfood(String ID) {
+        restaurant_now.addFood(Integer.parseInt(ID));
+        if(Restaurant.getRestaurant(Integer.parseInt(ID))==null){
+            return Message.NoRestaurant;
+        }
+        else{
+            return Message.SUCCESS;
+        }
+    }
+    }
 }
--- view/UserMenu.java
+++ view/UserMenu.java
@@ -7,6 +7,8 @@ import enums.Message;
 import Order.Restaurant;
 import LocationAndMap.map;
 
+import java.util.ArrayList;
+
 public class UserMenu extends Menu{
     static User user=null;
 
@@ -39,12 +41,61 @@ public class UserMenu extends Menu{
         this.Charge_Account();
         break;
         case "4":
+        case "Status":
+                this.showstatus();
+                break;
+        case "5":
         case "exit":
             this.exit();
             break;
         default:
         System.out.println(Message.INVALID_CHOICE);
+        this.run();
         }
+    }
+
+    private void showstatus() {
+        System.out.println("what do you want to do?");
+        System.out.println("1. confirm");
+        System.out.println("2. exit");
+        String choice = this.getChoice();
+
+        switch (choice) {
+            case "1":
+            case "Confirm":
+                this.controller.confirmorder();
+                this.time();
+                break;
+            case "2":
+            case "exit":
+                this.run();
+                break;
+            default:
+                System.out.println(Message.INVALID_CHOICE);
+        }
+        this.showstatus();
+    }
+
+    private void time() {
+        System.out.println("what do you want to do?");
+        System.out.println("1. time");
+        System.out.println("3. exit");
+        String choice = this.getChoice();
+
+        switch (choice) {
+            case "1":
+            case "time":
+                this.controller.showtime();
+
+                break;
+            case "2":
+            case "exit":
+                this.run();
+                break;
+            default:
+                System.out.println(Message.INVALID_CHOICE);
+        }
+        this.time();
     }
 
     private void showRestaurant() {
@@ -55,14 +106,190 @@ public class UserMenu extends Menu{
         for (int i = 0; i < Restaurant.restaurants.size(); i++) {
             System.out.println(Restaurant.restaurants.get(i));
         }
+        showRestaurantoption();
+        String choice = this.getChoice();
 
-        this.run();
+        switch (choice) {
+            case "1":
+            case "select":
+                this.showSelectRestaurant();
+                break;
+
+            case "2":
+            case "search":
+                this.searchrestaurant();
+                break;
+            case "3":
+            case "exit":
+                this.run();
+                break;
+            default:
+                System.out.println(Message.INVALID_CHOICE);
+        }
+        this.searchrestaurant();
+    }
+
+    private void showSelectRestaurant() {
+        showselectOptions();
+        String choice = this.getChoice();
+
+        switch (choice) {
+            case "1":
+            case "select":
+                String username = this.getInput("enter ID");
+
+                Message message = this.controller.selectrestaurant(username);
+                System.out.println(message == Message.SUCCESS ? "customer registered successfully" : message);
+                if (message == Message.SUCCESS){
+                    showMenu();
+                }
+                break;
+            case "2":
+            case "exit":
+                this.exit();
+                break;
+            default:
+                System.out.println(Message.INVALID_CHOICE);
+        }
+    }
+
+    private void showMenu() {
+        ArrayList<String> menu=controller.restaurant_now.getFood();
+        for (String ar:menu) {
+            System.out.println(ar);
+        }
+        showMenuOptions();
+        String choice = this.getChoice();
+
+        switch (choice) {
+            case "1":
+            case "select":
+                this.showSelectfood();
+                break;
+
+            case "2":
+            case "search":
+                this.searchmenu();
+                break;
+            case "3":
+            case "exit":
+                this.showRestaurant();
+                break;
+            default:
+                System.out.println(Message.INVALID_CHOICE);
+        }
+        this.searchrestaurant();
+
+    }
+
+    private void searchmenu() {
+        String username = this.getInput("enter Food name:");
+        ArrayList<String> message = this.controller.searchfood(username);
+        if (message.size()==0){
+            System.out.println(Message.Nofood);
+        }
+        else {
+            for (int i = 0; i < message.size(); i++) {
+                System.out.println(message.get(i));
+            }
+        }
+        showSelectfood();
+    }
+
+    private void showSelectfood() {
+        showselectOptions();
+        String choice = this.getChoice();
+
+        switch (choice) {
+            case "1":
+            case "select":
+                String username = this.getInput("enter ID");
+
+                Message message = this.controller.selectfood(username);
+                System.out.println(message == Message.SUCCESS ? "customer registered successfully" : message);
+                if (message == Message.SUCCESS){
+                    this.run();
+                }
+                else{
+                    this.showSelectfood();
+                }
+                break;
+            case "2":
+            case "exit":
+                this.exit();
+                break;
+            default:
+                System.out.println(Message.INVALID_CHOICE);
+        }
+    }
+
+    private void showMenuOptions() {
+        System.out.println("what do you want to do?");
+        System.out.println("1. select");
+        System.out.println("1. search");
+        System.out.println("3. exit");
+    }
+
+    private void showselectOptions() {
+        System.out.println("what do you want to do?");
+        System.out.println("1. select");
+        System.out.println("3. exit");
+    }
+
+    private void showRestaurantoption() {
+        System.out.println("what do you want to do?");
+        System.out.println("1. select");
+        System.out.println("2. search");
+        System.out.println("3. exit");
+    }
+
+    private void searchrestaurant() {
+        String username = this.getInput("enter Restaurant name:");
+        ArrayList<String> message = this.controller.searchrestaurant(username);
+        if (message.size()==0){
+            System.out.println(Message.NoRestaurant);
+        }
+        else {
+            for (int i = 0; i < message.size(); i++) {
+                System.out.println(message.get(i));
+                
+            }
+        }
+        showSelectRestaurant();
     }
 
     private void Showorders() {
+        
     }
 
     private void Charge_Account() {
+        controller.user.showmoney();
+        System.out.println("what do you want to do?");
+        System.out.println("1. charge");
+        System.out.println("2. exit");
+        String choice = this.getChoice();
+
+        switch (choice) {
+            case "1":
+            case "charge":
+                this.charge();
+                this.Charge_Account();
+                break;
+            case "2":
+            case "exit":
+                this.run();
+                break;
+            default:
+                System.out.println(Message.INVALID_CHOICE);
+        }
+        this.Charge_Account();
+    }
+
+    private void charge() {
+        String money = this.getInput("enter money:");
+
+        Message message = this.controller.charge(Integer.parseInt(money));
+        System.out.println(message == Message.SUCCESS ? "account charged succesfully" : message);
     }
 
     private void exit() {
